d9fca1322e67bd36dc20d5302ef0d590a657d540
srinadhtadikonda <44929625+srinadhtadikonda@users.noreply.github.com>
2022-12-19 00:55:51 +0530

    first commit

---
+++ Test39.java
@@ -0,0 +1,21 @@
+
+public class Test39
+{
+	public static void main(String args[])
+	{
+		int a[]=new int[5];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+int i;
+for(i=0;i<a.length;i++)
+{
+	System.out.println("\t"+a[i]);
+}
+	}
+
+}
+
+
