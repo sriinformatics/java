61385b116dafea83de180ed3576fd95121be361b
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2023-07-05 10:23:06 +0530

    Update Test46b.java

--- Test46b.java
+++ Test46b.java
@@ -1,10 +1,12 @@
-public class Main
+#jagged Array
+
+class Test
 {
 	public static void main(String[] args) 
 	{
-int a[][] = new int[3][3];
-    a[0]=new int[]{1,2,3};
-    a[1]=new int[]{10,20,30};
+int a[][] = new int[3][];
+    a[0]=new int[]{1,2,3,11};
+    a[1]=new int[]{10,20,30,45,67,78};
     a[2]=new int[]{5,7,9};
     
     
